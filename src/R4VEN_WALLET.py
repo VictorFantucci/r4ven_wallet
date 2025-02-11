471b8b4d284ba31fcac77510bdb21152937fa7f3
Victor Vinci Fantucci <victor.v.fantucci@gmail.com>
2025-02-10 22:50:54 -0300

    style: Change the page title

--- src/R4VEN_WALLET.py
+++ src/R4VEN_WALLET.py
@@ -1,10 +1,14 @@
 import os
+import sys
 import streamlit as st
 from PIL import Image
 
+app_directory =  os.path.dirname(os.path.dirname(__file__))
+sys.path.append(app_directory)
+
 def app() -> None:
     # Change page name and icon
-    page_icon = Image.open('src/imgs/r4ven_icon.png')
+    page_icon = Image.open(os.path.join(app_directory, 'src', 'imgs', 'r4ven_icon.png'))
 
     page_config = {'page_title': 'r4ven_wallet',
                    'page_icon': page_icon,
@@ -12,7 +16,7 @@ def app() -> None:
 
     st.set_page_config(**page_config)
 
-    st.title('Carteira - R4VEN')
+    st.title('R4VEN - Independência Financeira')
 
 if __name__ == '__main__':
     app()
