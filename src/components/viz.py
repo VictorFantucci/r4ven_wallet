498c4e9de4fe54c1e46cb1557e7701bbd7490a5b
Victor Vinci Fantucci <victor.v.fantucci@gmail.com>
2025-02-11 22:36:37 -0300

    feat: Add r4ven_line_plot method to the DataVisualizer class

--- src/components/viz.py
+++ src/components/viz.py
@@ -35,3 +35,53 @@ class DataVisualizer:
             "#ffb86c",  # Orange
             ]
 
+    def r4ven_line_plot(self,
+                        df: pd.DataFrame,
+                        x_data: str,
+                        y_data: Union[str, List[str]],
+                        dataframe_name: str,
+                        ylabel: Union[str, None] = None,
+                        title: str = None) -> go.Figure:
+        """
+        Generate and display a line plot using Plotly Express.
+
+        Args:
+            df (pd.DataFrame): The DataFrame containing the data to be plotted.
+            x_data (str): The column name in the DataFrame to be used as the x-axis data.
+            y_data (Union[str, List[str]]): The column name(s) in the DataFrame to be used as the y-axis data.
+                It can be a single column name as a string or a list of column names.
+            dataframe_name (str): The name of the DataFrame.
+            ylabel (Union[str, None]): The label for the y-axis. If None, the label will not be set.
+            title (str): The title for the plot. If None, the title will not be set.
+
+        Returns:
+            go.Figure: The Plotly figure containing the line plot.
+        """
+
+        # Create a logger object for this function
+        logger = self.get_logger()
+
+        try:
+            # Define a list of colors for lines
+            line_colors = self.get_dracula_color_palette()
+
+            # Creating a line plot with Plotly Express
+            fig = px.line(df, x=x_data, y=y_data, title=f'Line Plot of the {dataframe_name}',
+                          color_discrete_sequence=line_colors)
+
+            # Updating the traces of the plot to display both lines and markers
+            fig.update_traces(mode='lines+markers')
+
+            # Adjust y-axis range to ensure 0 is always visible
+            fig.update_yaxes(range=[0, max(df[y_data].max() * 1.1)])  # Adjust multiplier as needed
+
+            # Setting the label for the y-axis if ylabel is provided and y_data is a single string
+            if isinstance(y_data, str) and ylabel is not None:
+                fig.update_yaxes(title=title)
+
+            # Return the plot
+            return fig
+
+        except Exception as e:
+            logger.error(f"Data visualization failed: {e}")
+            raise
