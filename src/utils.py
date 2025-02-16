aa740c971273a580fb636b9847d1fd0a876555dc
Victor Fantucci <victor.v.fantucci@gmail.com>
2025-02-16 11:36:34 -0300

    refactor: Move load functions to data/load_data.py file

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,8 @@
+"""
+Script that contains utility functions used across multiple files of the project.
+"""
+
 import os
-import sys
-import pandas as pd
-
-# Load relative imports
-from data.gspread_fetcher import GoogleSheetsReader
-
-# Load environment variables
-from dotenv import load_dotenv, find_dotenv
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
 
 utils_directory =  os.path.dirname(os.path.dirname(__file__))
 
@@ -20,58 +14,3 @@ def get_src_folder() -> str:
         str: The absolute path to the src folder.
     """
     return os.path.join(utils_directory, "src")
-
-def load_transaction_data():
-    """Fetch transaction log and preprocess data."""
-    try:
-        logs_folder = get_src_folder()
-        reader = GoogleSheetsReader('credentials.json', logs_folder)
-
-        sheet_id = os.environ.get('wallet_sheet_id')
-        worksheet_id = os.environ.get('log_transactions_worksheet_id')
-
-        if not sheet_id or not worksheet_id:
-            raise ValueError("Environment variables for sheet ID or worksheet ID are missing.")
-
-        df = reader.get_sheet_as_dataframe(sheet_id, worksheet_id)
-
-        # Convert columns
-        df["Data Negócio"] = pd.to_datetime(df["Data Negócio"], errors="coerce")
-        df["Preço (R$)"] = pd.to_numeric(df["Preço (R$)"], errors="coerce")
-        df["Preço Total (R$)"] = pd.to_numeric(df["Preço Total (R$)"], errors="coerce")
-
-        # Create month column
-        df["Mês"] = df["Data Negócio"].dt.to_period("M").astype(str)
-
-        return df
-
-    except Exception as e:
-        print(f"Error loading transaction data: {e}")
-        return None
-
-def load_passive_income_data():
-    """Fetch transaction log and preprocess data."""
-    try:
-        logs_folder = get_src_folder()
-        reader = GoogleSheetsReader('credentials.json', logs_folder)
-
-        sheet_id = os.environ.get('wallet_sheet_id')
-        worksheet_id = os.environ.get('log_passive_income_worksheet_id')
-
-        if not sheet_id or not worksheet_id:
-            raise ValueError("Environment variables for sheet ID or worksheet ID are missing.")
-
-        df = reader.get_sheet_as_dataframe(sheet_id, worksheet_id)
-
-        # Convert columns
-        df["Data"] = pd.to_datetime(df["Data"], errors="coerce")
-        df["Valor Líquido (R$)"] = pd.to_numeric(df["Valor Líquido (R$)"], errors="coerce")
-
-        # Create month column
-        df["Mês"] = df["Data"].dt.to_period("M").astype(str)
-
-        return df
-
-    except Exception as e:
-        print(f"Error loading passive income data: {e}")
-        return None
