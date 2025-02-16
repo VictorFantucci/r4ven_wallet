ebc75e53724326563346ee2240a69c7e821032ae
Victor Fantucci <victor.v.fantucci@gmail.com>
2025-02-16 17:23:40 -0300

    feat: Add class AssetsDataLoader to load info specific to each type of asset in my wallet

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,3 +1,8 @@
+"""
+Script that contains classes used to load multiple sheets and worksheets from my Google Drive.
+"""
+
+# Load imports
 import os
 import logging
 import pandas as pd
@@ -15,7 +20,6 @@ from data.gspread_fetcher import GoogleSheetsReader
 from dotenv import load_dotenv, find_dotenv
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
-
 
 # Log data
 class LogsDataLoader:
@@ -256,3 +260,137 @@ class WalletDataLoader:
             return df_goal  # Return the preprocessed goal data
         else:
             return None  # Return None if data fetching fails
+
+#
+class AssetsDataLoader:
+    def __init__(self, logs_folder):
+        """
+        Initializes the AssetsDataLoader instance with the given logs folder path.
+
+        Args:
+            logs_folder (str): Path to the folder where logs are stored.
+        """
+        self.credentials_file = 'credentials.json'  # File containing credentials for Google Sheets API
+        self.logs_folder = logs_folder  # Folder to store logs
+        self.reader = GoogleSheetsReader(self.credentials_file, self.logs_folder)  # Google Sheets reader instance
+
+        # Initialize log manager for logging
+        self.log_manager = r4venLogManager(logs_folder)
+
+    def get_logger(self):
+        """
+        Helper method to get a logger dynamically for the current file.
+
+        Returns:
+            Logger instance for logging.
+        """
+        return self.log_manager.function_logger(__file__, console_level=logging.INFO)
+
+    def _fetch_data(self) -> pd.DataFrame:
+        """
+        Fetches data from the Google Sheets document.
+
+        Returns:
+            pd.DataFrame: Data fetched from the specified sheet and worksheet.
+            None: If an error occurs while fetching data.
+        """
+        logger = self.get_logger()  # Get logger for error reporting
+        try:
+            # Fetch the sheet as a DataFrame
+            df = self.reader.get_sheet_as_dataframe(self.sheet_id, self.worksheet_id, True)
+            return df  # Return the fetched DataFrame
+        except Exception as e:
+            # Log any error encountered while fetching data
+            logger.error(f"Error fetching data: {e}")
+            return None  # Return None if error occurs
+
+    def _load_data_from_sheet(self, sheet_id, worksheet_id, numeric_columns):
+        """
+        Loads and preprocesses data from a Google Sheet.
+
+        Args:
+            sheet_id (str): ID of the Google Sheets document.
+            worksheet_id (str): ID of the worksheet in the sheet.
+            date_column (str): Name of the column containing date values.
+            numeric_columns (list): List of columns containing numeric values to convert.
+
+        Returns:
+            pd.DataFrame: Preprocessed data in a DataFrame format.
+            None: If an error occurs while loading or processing data.
+        """
+        logger = self.get_logger()  # Get logger for error reporting
+        try:
+            # Ensure sheet and worksheet IDs are provided
+            if not sheet_id or not worksheet_id:
+                raise ValueError("Environment variables for sheet ID or worksheet ID are missing.")
+
+            # Fetch data from the Google Sheet
+            df = self.reader.get_sheet_as_dataframe(sheet_id, worksheet_id)
+
+            # Convert specified columns to numeric
+            for column in numeric_columns:
+                df[column] = pd.to_numeric(df[column], errors="coerce")
+
+            return df  # Return the preprocessed DataFrame
+
+        except Exception as e:
+            # Log any error encountered
+            logger.error(f"Error loading data from sheet {worksheet_id}: {e}")
+            return None  # Return None if error occurs
+
+    def load_asset_stocks_data(self):
+        """
+        Fetches and preprocesses stocks data.
+
+        Returns:
+            pd.DataFrame: Preprocessed stocks data.
+        """
+        sheet_id = os.environ.get('wallet_sheet_id')  # Fetch sheet ID from environment variables
+        worksheet_id = os.environ.get('asset_stocks_worksheet_id')  # Fetch worksheet ID for transactions
+
+        # Define numeric columns
+        numeric_columns = ['% Ideal', 'Quantidade',
+                           'Total (R$)', 'Cotação (R$)',
+                           'Preço Médio (R$)', '% Atual',
+                           'Meta (R$)', 'Falta (R$)',
+                           'Sugestão']
+
+        return self._load_data_from_sheet(sheet_id, worksheet_id, numeric_columns)
+
+    def load_asset_real_estate_data(self):
+        """
+        Fetches and preprocesses real estate data.
+
+        Returns:
+            pd.DataFrame: Preprocessed real estate data.
+        """
+        sheet_id = os.environ.get('wallet_sheet_id')  # Fetch sheet ID from environment variables
+        worksheet_id = os.environ.get('asset_real_estate_worksheet_id')  # Fetch worksheet ID for transactions
+
+        # Define numeric columns
+        numeric_columns = ['% Ideal', 'Quantidade',
+                           'Total (R$)', 'Cotação (R$)',
+                           'Preço Médio (R$)', '% Atual',
+                           'Meta (R$)', 'Falta (R$)',
+                           'Sugestão']
+
+        return self._load_data_from_sheet(sheet_id, worksheet_id, numeric_columns)
+
+    def load_asset_small_caps_data(self):
+        """
+        Fetches and preprocesses small caps data.
+
+        Returns:
+            pd.DataFrame: Preprocessed small caps data.
+        """
+        sheet_id = os.environ.get('wallet_sheet_id')  # Fetch sheet ID from environment variables
+        worksheet_id = os.environ.get('asset_small_caps_worksheet_id')  # Fetch worksheet ID for transactions
+
+        # Define numeric columns
+        numeric_columns = ['% Ideal', 'Quantidade',
+                           'Total (R$)', 'Cotação (R$)',
+                           'Preço Médio (R$)', '% Atual',
+                           'Meta (R$)', 'Falta (R$)',
+                           'Sugestão']
+
+        return self._load_data_from_sheet(sheet_id, worksheet_id, numeric_columns)
