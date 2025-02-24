c4145707ffcc6d3561572d840a2293662f630463
Victor Fantucci <victor.v.fantucci@gmail.com>
2025-02-24 20:49:00 -0300

    feat: Add wallet general info to main page

--- src/R4VEN_WALLET.py
+++ src/R4VEN_WALLET.py
@@ -20,30 +20,49 @@ import streamlit as st
 
 def investment_goals(current_value: float) -> None:
     """
-    Displays a list of financial goals in Markdown format, striking through those already achieved.
+    Displays a list of financial goals in two columns in Markdown format,
+    striking through those already achieved.
 
     Args:
         current_value (float): The current investment value.
     """
 
-    # List of financial goals with their respective values formatted in Brazilian Real (R$)
+    # List of financial goals
     goals = [
+        (25000, 'R$ 25,000'),
         (50000, 'R$ 50,000'),
+        (75000, 'R$ 75,000'),
         (100000, 'R$ 100,000'),
+        (250000, 'R$ 250,000'),
         (500000, 'R$ 500,000'),
+        (750000, 'R$ 750,000'),
         (1000000, 'R$ 1,000,000'),
         (2500000, 'R$ 2,500,000'),
         (5000000, 'R$ 5,000,000'),
+        (7500000, 'R$ 7,500,000'),
+        (10000000, 'R$ 10,000,000')
     ]
 
-    # Creates a Markdown text where achieved goals are struck through using '~~'
-    markdown_text = "\n".join(
-        f"- {'~~' + text + '~~' if current_value >= amount else text}"
-        for amount, text in goals
-    )
+    # Split goals into two halves for the columns
+    mid_index = len(goals) // 2
+    goals_col1 = goals[:mid_index]
+    goals_col2 = goals[mid_index:]
 
-    # Displays the formatted text in Streamlit
-    st.markdown(markdown_text)
+    # Create two columns in Streamlit
+    col1, col2 = st.columns(2)
+
+    # Generate Markdown text for each column
+    with col1:
+        st.markdown("\n".join(
+            f"- {'~~' + text + '~~' if current_value >= amount else text}"
+            for amount, text in goals_col1
+        ))
+
+    with col2:
+        st.markdown("\n".join(
+            f"- {'~~' + text + '~~' if current_value >= amount else text}"
+            for amount, text in goals_col2
+        ))
 
 # Main function
 def app() -> None:
@@ -85,6 +104,11 @@ def app() -> None:
     with col1:
         st.subheader('Meta Atual da Carteira')
         st.dataframe(wallet_goal, hide_index=True)
+        st.markdown("""
+                    _**Quem não sabe onde quer chegar, qualquer lugar serve, ATÉ LUGAR NENHUM**._
+
+                    > Prof. Mira
+                    """)
     with col2:
         st.subheader('Metas a bater:')
         investment_goals(wallet_goal.iloc[0, 1])
@@ -92,11 +116,13 @@ def app() -> None:
     # Wallet Overview
     st.subheader('Resultado da Carteira')
     wallet_overview = wallet_loader.load_wallet_overview()
+    wallet_overview.loc[:, ['Variação (%)']] *= 100
     st.dataframe(wallet_overview, hide_index=True)
 
     # Wallet Division
     st.subheader("Alocação da Carteira")
     wallet_division = wallet_loader.load_wallet_division()
+    wallet_division.loc[:, ['% Ideal', '% Atual']] *= 100
     st.dataframe(wallet_division, hide_index=True)
 
 if __name__ == '__main__':
